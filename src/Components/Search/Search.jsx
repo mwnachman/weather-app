56e41ed2b3818c84f90830b9a9c0c55bebbe1cfe
Meredith Nachman <mwnachman@gmail.com>
2017-02-06 00:32:07 -0800

    make input field clear on submit

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -23,14 +23,28 @@ const mapDispatchToProps = (dispatch) => {
 
 class Search extends Component {
 
-  // ADD AN EVENT HANDLER TO CLEAR THE TEXT OF THE INPUT
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: ''
+    };
+  }
+
+  handleInputChange(e) {
+    const inputChange = {};
+    inputChange[e.target.name] = e.target.value;
+    this.setState(inputChange);
+  }
 
   findCity(city) {
     if ( parseInt(city) === Number(city) ) {
       this.props.findCityByZip(city);
+      this.setState({ city: '' });
     } else {
       let parsedCity = parseCityName(city);
       this.props.findCity(parsedCity[0], parsedCity[1]); 
+      this.setState({ city: '' });
     }
   }
 
@@ -42,9 +56,11 @@ class Search extends Component {
           e.preventDefault();
           this.findCity(e.target.elements[0].value);
         }}>
-          <input placeholder='' type='text' name='city'/>
+          <input placeholder="" type="text" name="city"
+            value={this.state.city}
+            onChange={(e) => this.handleInputChange(e)}></input>
           <p className="example-text">(e.g., "Boulder, CO" or "80301")</p>
-          <button type='submit'>
+          <button type="submit">
             Submit 
           </button>
         </form>
